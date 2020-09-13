1ba6c3f04c18e811926bc224c9e171ce4e89af7b
tatoshka-toxa <71187646+tatoshka-toxa@users.noreply.github.com>
2020-09-13 21:54:36 +0300

    Финальная версия

--- src/test/java/roseltorg/TestBase.java
+++ src/test/java/roseltorg/TestBase.java
@@ -11,6 +11,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBase {
 
+   public String company = "росэлторг";
+   public By yaInput = By.id("text");
+   public By find = By.xpath("//button[@type='submit']");
+   public By site = By.xpath("//*[text()='Единая электронная торговая площадка (']");
+   public By reg = By.xpath("//header/div/div/button");
+   public By post = By.xpath("//img[@alt='Поставщик']");
+   public By etp = By.linkText("Единая аккредитация для участия в корпоративных закупках и закупках субъектов 223-ФЗ, аккредитация иностранных поставщиков (в том числе для регистрации в ЕРУЗ)");
+   public By fin = By.xpath("//b[text()='Если Вы приобрели новую электронную подпись в связи с истечением срока действия старой, то Вам следует загрузить ее в профиле зарегистрированного пользователя, а не регистрировать нового пользователя']");
+
+
    public WebDriver driver = new ChromeDriver();
 
    @Before
@@ -21,6 +31,12 @@ public class TestBase {
    @After
    public void tearDown() {
       driver.quit();
+   }
+
+   public void textInput(WebDriver driver, By locator, String text) {
+      clickAbility(driver, locator);
+      driver.findElement(locator).clear();
+      driver.findElement(locator).sendKeys(text);
    }
 
    public void clickAbility(WebDriver driver, By xpath) {
--- src/test/java/roseltorg/TestRosElTorg.java
+++ src/test/java/roseltorg/TestRosElTorg.java
@@ -5,20 +5,10 @@ import org.openqa.selenium.By;
 
 public class TestRosElTorg extends TestBase {
 
-   private By find = By.xpath("//button[@type='submit']");
-   private By site = By.xpath("//*[text()='Единая электронная торговая площадка (']");
-   private By reg = By.xpath("//header/div/div/button");
-   private By post = By.xpath("//img[@alt='Поставщик']");
-   private By etp = By.linkText("Единая аккредитация для участия в корпоративных закупках и закупках субъектов 223-ФЗ, аккредитация иностранных поставщиков (в том числе для регистрации в ЕРУЗ)");
-   private By fin = By.xpath("//b[text()='Если Вы приобрели новую электронную подпись в связи с истечением срока действия старой, то Вам следует загрузить ее в профиле зарегистрированного пользователя, а не регистрировать нового пользователя']");
-
-
    @Test
    public void signUp() throws InterruptedException {
 
-      driver.findElement(By.id("text")).click();
-      driver.findElement(By.id("text")).clear();
-      driver.findElement(By.id("text")).sendKeys("росэлторг");
+      textInput(driver, yaInput, company);
 
       clickAbility(driver, find);
       clickAbility(driver, site);
